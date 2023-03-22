71bd3037d0358fd92090fe1d1905c1dd84cfc2ba
Piero <piero.migliorato@studenti.unicampania.it>
2023-03-22 11:37:00 +0100

    plot images line

---
+++ plot_images.py
@@ -0,0 +1,17 @@
+        # plt.hlines(e/N, 0, 100, linestyles='dashdot', label='e/N')
+        # plt.plot(c, 'r-.', label='convergents')
+        # plt.scatter(x=[range(0, len(c))], y=c, label='convergent points')
+        # plt.legend()
+        # plt.axis([0, len(a), 0.1, 1.1])
+        # plt.show()
+
+        # pi_exp = [3,7,15,1,292,1,1,1,2]
+        # c_pi, num_pi, den_pi = convergents(pi_exp)
+
+        # plt.hlines(math.pi, 0, 100, linestyles='dashdot', label='pi')
+        # plt.plot(c_pi, 'r-.', label='convergents of pi')
+        # plt.scatter(x=[range(0, len(c_pi))], y=c_pi )
+        # plt.legend()
+        # plt.axis([0, len(pi_exp), 2.9, 3.2])
+        # print(c_pi)
+        # plt.show()
